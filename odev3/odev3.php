60e8b9fa0381df1e2a71aaaf5a000319139f04ac
namik <namik@NAMIKDOLAMAN.onduline.com.tr>
2014-04-15 16:58:25 +0300

    odev3 tamamlandi.

---
+++ odev3/odev3.php
@@ -0,0 +1,45 @@
+<?php
+
+echo "-Sifre Uret-";
+echo "<br><br>";
+echo $sifre="Basit Sifre: " . basit_sifre_uret();
+echo "<br><br>";
+echo $sifre2 = "Guclu Sifre: " . guclu_sifre_uret();
+function basit_sifre_uret($uzunluk = 10)
+{
+    $sifre= "";
+    $karakter1 = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+    if($uzunluk <= 3)
+    {
+        echo "Sifre 3 karakterden uzun olmali";
+    }
+    else{
+    for($i=1;$i<$uzunluk+1;$i++)
+    {
+       $sifre.= $karakter1{
+           rand()%52
+           };
+       
+    }
+    return $sifre;
+}}
+function guclu_sifre_uret($uzunluk = 10)
+{
+    
+    $sifre2 = "";
+    $karakter2 = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ+-/?*!0123456789";
+    if($uzunluk <= 3)
+    {
+        echo "Sifre 3 karakterden uzun olmali";
+    }
+    else{
+    for($j=1;$j<$uzunluk+1;$j++)
+    {
+        $sifre2.= $karakter2{
+            rand()%68
+            };
+        
+    }
+    return $sifre2;
+    }
+}
